84ed9debc988ed664f278d3dad663ca628e94825
Rodrigo Alcala <rodri.a90@gmail.com>
2019-02-27 22:04:10 +0100

    feat: render map

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    barcelona: {lat: 41.390205, lng: 2.154007}
+  }
+
+  componentDidMount() {
+    this.renderMap();
+  }
+
+  renderMap = () => {
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyD8CW2x7KfEWmzWTge0_kfpYI6QsBe90bU&callback=initMap');
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    new window.google.maps.Map(document.getElementById('map'), {
+      center: this.state.barcelona,
+      zoom: 12
+    })
+  }
+
   render() {
     return (
       <div className="App">
+        <div id="map"></div>
         <h1>tempus</h1>
       </div>
     );
   }
+}
+
+// Needed to load the script at the very top of our scripts
+function loadScript(url) {
+  const index  = window.document.getElementsByTagName("script")[0];
+  const script = window.document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
