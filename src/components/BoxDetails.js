3ca0c4f4af64c317cd3331e2a70f277335b90a6f
Rodrigo Alcala <rodri.a90@gmail.com>
2019-02-28 19:28:04 +0100

    style: add SIM type to BoxDetails

--- src/components/BoxDetails.js
+++ src/components/BoxDetails.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './App.css';
 
 const BoxDetails = ({ box }) => (
-  <div className="box-details">
-    <h1>{box.name}</h1>
-    <h3 className="data">{box.dataLeft}</h3>
-    <h3 className="mins">{box.minsLeft}</h3>
-    <h4>{box.expiration}</h4>
+  <div className="details-conteiner">
+    <div className="sim-details">
+      <h1>{box.name}</h1>
+      <h3 className="data">{box.dataLeft + ' Mb of data available'}</h3>
+      <h3 className="mins">{box.minsLeft + ' free mins available'}</h3>
+      <h4>{'Expiring on the ' + (new Date(box.expiration)).getDate() + ' of ' + (new Date(box.expiration)).getMonth()}</h4>
+    </div>
+    <div className="sim-type">
+      <img className="sim-image" src={require('./../assets/sizes-original.png')} alt="SIM sizes"/>
+    </div>
   </div>
 )
 
