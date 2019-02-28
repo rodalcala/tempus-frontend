dcc8ea04f7f59ca06cdd02d2dcc1b0232acb1940
Rodrigo Alcala <rodri.a90@gmail.com>
2019-02-28 12:36:40 +0100

    feat: add boxes to the map

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import './App.css';
 class App extends Component {
 
   state = {
-    barcelona: {lat: 41.390205, lng: 2.154007},
+    barcelona: {lat: 41.397759, lng: 2.187349},
     baseUrl: 'http://localhost:4000',
   }
 
   componentDidMount() {
-    this.renderMap();
     this.getBoxes();
+    this.renderMap();
   }
 
   getBoxes() {
@@ -27,7 +27,7 @@ class App extends Component {
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: this.state.barcelona,
-      zoom: 12
+      zoom: 14
     });
     // let infoWindow = new window.google.maps.InfoWindow();
 
@@ -58,6 +58,39 @@ class App extends Component {
       // Browser doesn't support Geolocation
       this.handleLocationError(false);
     }
+
+    let fullBox = {
+      url: require('./../assets/simcard.png'),
+      size: new window.google.maps.Size(32, 32),
+      origin: new window.google.maps.Point(0, 0),
+      anchor: new window.google.maps.Point(16, 16)
+    }
+
+    let emptyBox = {
+      url: require('./../assets/open-box.png'),
+      size: new window.google.maps.Size(32, 32),
+      origin: new window.google.maps.Point(0, 0),
+      anchor: new window.google.maps.Point(16, 16)
+    }
+
+    // add boxes to the map
+    if (this.state.boxes) {
+      this.state.boxes.forEach((box) => {
+        if (box.full) {
+          new window.google.maps.Marker({
+            position: {lat: box.lat, lng: box.lng},
+            map: map,
+            icon: fullBox,
+          })
+        } else {
+          new window.google.maps.Marker({
+            position: {lat: box.lat, lng: box.lng},
+            map: map,
+            icon: emptyBox,
+          })
+        }
+      })
+    }
   }
 
   handleLocationError = (browserHasGeolocation) => {
@@ -70,7 +103,6 @@ class App extends Component {
     return (
       <div className="App">
         <div id="map"></div>
-        <h1>tempus</h1>
       </div>
     );
   }
