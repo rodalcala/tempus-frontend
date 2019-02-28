c1104f684ab9f1ab6f932e12aa6826db5cbc5569
Rodrigo Alcala <rodri.a90@gmail.com>
2019-02-28 17:59:10 +0100

    style: basic css on BoxDetails

--- src/components/BoxDetails.js
+++ src/components/BoxDetails.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 
 const BoxDetails = ({ box }) => (
-  <div className="berta">
-    {box.name}
+  <div className="box-details">
+    <h1>{box.name}</h1>
+    <h3 className="data">{box.dataLeft}</h3>
+    <h3 className="mins">{box.minsLeft}</h3>
+    <h4>{box.expiration}</h4>
   </div>
 )
 
