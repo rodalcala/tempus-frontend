8cfc53ce816871ba7d637d29c6d56885c608b9d5
Rodrigo Alcala <rodri.a90@gmail.com>
2019-03-05 22:35:08 +0100

    refactor: change ListView styling to show same data as BoxPreview

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -32,6 +32,46 @@ class ListView extends Component {
       .then(boxes => this.setState({ boxes }))
   }
 
+  renderSimType = (box) => {
+    const { simType } = box;
+    if (simType === 'SIM') {
+      return <img className="Map-sim-image" src={require('./../assets/sizes-SIM.png')} alt="SIM size"/>
+    } else if (simType === 'microSIM') {
+      return <img className="Map-sim-image" src={require('./../assets/sizes-microSIM.png')} alt="microSIM size"/>
+    } else if (simType === 'nanoSIM') {
+      return <img className="Map-sim-image" src={require('./../assets/sizes-nanoSIM.png')} alt="nanoSIM size"/>
+    } else {
+      return <img className="Map-add-image" src={require('./../assets/plus.png')} alt="add"/>
+    }
+  }
+
+  renderExpiration = (box) => {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const expiration = new Date(box.expiration);
+    const lastNum = (expiration.getDate()).toString().split('').pop();
+    let sufix;
+    if (lastNum === '1') {
+      sufix = 'st';
+    } else if (lastNum === '2') {
+      sufix = 'nd';
+    } else if (lastNum === '3') {
+      sufix = 'rd';
+    } else {
+      sufix = 'th';
+    }
+    if (box.expiration) {
+      return (
+        <div>
+          <h3 className="ListView-data">{box.dataLeft + ' Mb of data available'}</h3>
+          <h3 className="ListView-mins">{box.minsLeft + ' free mins available'}</h3>
+          <h4>{'Expiring on the ' + expiration.getDate() + sufix + ' of ' + months[expiration.getMonth()]}</h4>
+        </div>
+      )
+    } else {
+      return <h2>Empty box ready to<br />be filled up with 💞<br /><p>(and SIM cards)</p></h2> // eslint-disable-line
+    }
+  }
+
   renderList = () => {
     return this.state.boxes.map(box => (
       <Link to={{
@@ -40,7 +80,13 @@ class ListView extends Component {
           box: box
         }
       }} className="ListView-box-container">
-        {box.name}
+        <div className="ListView-sim-details">
+          <h1>{box.name}</h1>
+          <div>{this.renderExpiration(box)}</div>
+        </div>
+        <div className="ListView-sim-type">
+          {this.renderSimType(box)}
+        </div>
       </Link>
     ))
   }
@@ -51,6 +97,8 @@ class ListView extends Component {
         <div className="ListView-header-container" />
         <div className="ListView-boxes-container">
           {this.state.boxes ? this.renderList() : 'Loading...'}
+        </div>
+        <div>
         </div>
       </div>
     );
